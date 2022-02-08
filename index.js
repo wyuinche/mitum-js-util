3e5f93afef62ff0c37530b722547d1d3b01a1a35
wyuinche <wyuinche.mido@gmail.com>
2022-02-08 15:34:06 +0900

    Add block-city; change all method prefix create(deprecated) -> get

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const { Signer } = require('./lib/sign');
 
 const { getNewKeypair, getKeypairFromPrivateKey, getKeypairFromSeed } = require('./lib/key');
 
-const { BLOCKSIGN_CREATE_DOCUMENTS, BLOCKSIGN_SIGN_DOCUMENTS, BLOCKSIGN_TRANSFER_DOCUMENTS } = require('./lib/operation/blocksign');
+const { BLOCKSIGN_CREATE_DOCUMENTS, BLOCKSIGN_SIGN_DOCUMENTS } = require('./lib/operation/blocksign');
 const BlockSignType = {
     BLOCKSIGN_CREATE_DOCUMENTS,
     BLOCKSIGN_SIGN_DOCUMENTS,
-    BLOCKSIGN_TRANSFER_DOCUMENTS
+};
+
+const { DOCTYPE_USER_DATA, DOCTYPE_LAND_DATA, DOCTYPE_VOTE_DATA } = require('./lib/operation/blockcity');
+const BlockCityDocType = {
+    DOCTYPE_USER_DATA,
+    DOCTYPE_LAND_DATA,
+    DOCTYPE_VOTE_DATA
 };
 
 module.exports = {
@@ -20,4 +26,5 @@ module.exports = {
     getKeypairFromSeed,
     
     BlockSignType,
+    BlockCityDocType,
 };
